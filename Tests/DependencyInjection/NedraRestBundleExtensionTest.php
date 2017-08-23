e0c02c2068e93e4986e8817db815f4c2731fbd4b
Mert Oksuz <xrefresh@gmail.com>
2017-08-24 01:56:08 +0300

    ResourceController test

--- Tests/DependencyInjection/NedraRestBundleExtensionTest.php
+++ Tests/DependencyInjection/NedraRestBundleExtensionTest.php
@@ -2,14 +2,47 @@
 namespace Nedra\RestBundle\Tests\DependencyInjection;
 
 use FOS\RestBundle\DependencyInjection\FOSRestExtension;
+use Nedra\RestBundle\Controller\ResourceController;
 use Nedra\RestBundle\DependencyInjection\NedraRestExtension;
 use Nedra\RestBundle\NedraRestBundle;
 use Symfony\Cmf\Bundle\RoutingBundle\DependencyInjection\CmfRoutingExtension;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Nedra\RestBundle\Tests\DependencyInjection\Models\Book;
 
 
 class ResourceControllerTest extends TestCase
 {
+
+    /**
+     * @var \Symfony\Component\HttpKernel\Kernel
+     */
+    protected $kernel;
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\Container
+     */
+    protected $container;
+
+    /**
+     * @return null
+     */
+    public function setUp()
+    {
+        $this->kernel = new \AppKernel('test', true);
+        $this->kernel->boot();
+
+        $this->container = $this->kernel->getContainer();
+        $this->entityManager = $this->container->get('doctrine')->getManager();
+
+        parent::setUp();
+    }
+
     public function test_no_nedra_rest_bundle_when_active_is_false()
     {
         $container = ContainerFactory::createContainer("disabled.yml");
@@ -38,6 +71,60 @@ class ResourceControllerTest extends TestCase
     {
         $container = ContainerFactory::createContainer(null, [new CmfRoutingExtension()]);
         $this->assertTrue($container->hasExtension("cmf_routing"));
+    }
+
+    public function test_resource_controller()
+    {
+        $config = [
+            'nedra_rest' => [
+                'entities' => [
+                    'app.book' => [
+                        'classes' => [
+                            'model' => 'Nedra\RestBundle\Tests\DependencyInjection\Models\Test',
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $container = ContainerFactory::createDummyContainer();
+        $container->setParameter("nedrarest.config", $config);
+
+        $ext = new NedraRestExtension();
+        $ext->load($config, $container);
+        $container->registerExtension($ext);
+        $bundle = new NedraRestBundle();
+        $bundle->build($container);
+
+        $requestFactory = $container->get("nedra_rest.request_configuration_factory");
+        $registry = $container->get("nedra_rest.registry");
+
+        $container->register("nedra_rest.form_factory", "Nedra\RestBundle\Controller\RequestFormConfiguration")
+            ->addArgument($this->container->get("form.factory"))
+            ->addArgument($registry)
+            ->addArgument($this->entityManager);
+
+
+        $registry->addFromAliasAndConfiguration("app.book",
+            [
+                'driver' => 'doctrine',
+                'classes' => ['model' => 'Nedra\RestBundle\Tests\DependencyInjection\Models\Book']
+
+            ]
+        );
+
+        $req = new Request();
+        $req->attributes->add([
+            '_nedrarest' => [
+                'model' => 'Nedra\RestBundle\Tests\DependencyInjection\Models\Book'
+            ]
+        ]);
+
+        $controller = new ResourceController($registry, $requestFactory, $this->entityManager, $container->get("nedra_rest.form_factory"));
+
+        if ($controller) {
+            $this->assertTrue(true);
+        }
     }
 
     public function test_if_nedra_rest_active_then_create_routes_by_given_entities()
